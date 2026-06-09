c2425a8c61c28fb20a99e0e1135a326f597d49e8
ImNot <186264545+1mNut@users.noreply.github.com>
2026-06-09 22:47:46 +0200

    Open window

--- app.cpp
+++ app.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include <cmath>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
+GLFWwindow* StartGLFW();
+
 int main() {
-    return 0;
+    GLFWwindow* window = StartGLFW();
+
+    while(!glfwWindowShouldClose(window)) {
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+GLFWwindow* StartGLFW() {
+    if(!glfwInit()) {
+
+        cerr << "failed to initialize glfw << endl";
+        return nullptr;
+    }
+    GLFWwindow* window = glfwCreateWindow(1400, 900, "Gravity Simulation", NULL, NULL);
+
+    return window;
 }
